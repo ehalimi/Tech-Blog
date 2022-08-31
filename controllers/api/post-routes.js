fa3c691ffbc611d4e29d9540611d7fa7df543d41
Ema Halimi <ehalimi@na.edu>
2022-08-31 15:52:02 -0500

    added post and delete

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -61,3 +61,63 @@ router.get('/:id', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+router.post('/', (req, res) => {
+    // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+    Post.create({
+        title: req.body.title,
+        content: req.body.content,
+        user_id: req.session.user_id
+    })
+    .then(dbPostData => res.redirect(req.body.redirect || '/'))
+    .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+    });
+});
+
+router.post('/:id', (req, res) => {
+    Post.update(
+        {
+            title: req.body.title,
+            content: req.body.content
+        },
+        {
+            where: {
+            id: req.params.id
+            }
+        }
+    )
+    .then(dbPostData => {
+    if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+    }
+    res.redirect('/dashboard')
+    })
+    .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+    if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+    }
+    res.json(dbPostData);
+    })
+    .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+    });
+});
+
+module.exports = router;
